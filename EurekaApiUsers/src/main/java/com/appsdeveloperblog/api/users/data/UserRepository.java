855e1c6882e598f3d9fe7bc4b5e27816004bdf1b
tongtongyuanyuan <txue3@stevens.edu>
2020-06-11 21:58:10 -0400

    Added UserDetails

---
+++ EurekaApiUsers/src/main/java/com/appsdeveloperblog/api/users/data/UserRepository.java
@@ -0,0 +1,8 @@
+package com.appsdeveloperblog.api.users.data;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserRepository extends CrudRepository<UserEntity, Long> {
+	
+
+}
